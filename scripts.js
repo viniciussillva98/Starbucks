dd4fdf87861ce1b16aa02189b9163a77e5eab09e
viniciussillva98 <viniciussaldanha1998@gmail.com>
2025-04-28 03:26:29 -0300

    Primeiro-commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let imagem = document.querySelector(".imagem-cop")
+let circulo =document.querySelector(".circulo")
+    
+function trocaimagem(endereco){
+    imagem.src = endereco
+}
+function trocacor(cor){
+    circulo.style.background = cor
+}
